2f25f41e100191483f793689c4e03d995a12eb39
bigonil <luca.bigoni@gmail.com>
2024-10-04 16:53:40 +0200

    Add a version = ">= 5.70.0" and AWS Account ID

--- providers.tf
+++ providers.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 5.31.0"
+      version = ">= 5.70.0"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  profile = "Helen"
+  profile = "lb-aws-admin"
   # Configuration options
 }
 
