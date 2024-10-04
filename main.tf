b7fc5fde920fffeee84dc51a9e2ff650a03a5bd0
bigonil <luca.bigoni@gmail.com>
2024-10-04 21:28:36 +0200

    Update provider and add a option for import instance

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-resource "aws_instance" "MyInstance_test" {
+resource "aws_instance" "lb-tf-import-test-631737274131" {
     #instance configuration
+    ami="ami-0583d8c7a9c35822c"
+    instance_type = "t2.micro"
+    tags = {
+        "Name" = "lb-tf-import-test-631737274131"
+    }
 }
