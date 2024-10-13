015e2b967d6a2307117ce4600e3f013a355aaeb6
bigonil <luca.bigoni@gmail.com>
2024-10-13 11:11:58 +0200

    Formatt providers.tf

--- providers.tf
+++ providers.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 5.31.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "lb-aws-admin"
   # Configuration options
 }
 
 provider "aws" {
-  alias = "west"
-  region = "us-west-1"
+  alias   = "west"
+  region  = "us-west-1"
   profile = "lb-aws-admin"
 }
