fe36f450867ed47372b0d39020af2a7ee6a56752
bigonil <luca.bigoni@gmail.com>
2024-10-24 22:00:47 +0200

    Add skip_final_snapshot variable

--- main.tf
+++ main.tf
@@ -4,10 +4,11 @@ resource "aws_db_instance" "example-db-instance" {
   storage_type = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  username             = "admin"
+  instance_class       = "db.t3.micro"
+  username             = var.database_username
   password             = var.database_password
   parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot = true
   
 }
 
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "database_password" {
   description = "The password for the database"
   type        = string
 }
+
+variable "database_username" {
+  description = "The username for the database"
+  type        = string
+}
