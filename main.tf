f72c629a7b4f019d96e01915eebebb565a5620e9
bigonil <luca.bigoni@gmail.com>
2024-11-11 19:33:56 +0100

    Adjust profile to use and resource group to deploy to

--- main.tf
+++ main.tf
@@ -1,47 +1,41 @@
 
-# data "aws_vpc" "default-vpc" {
-#   default = true
-# }
+data "aws_vpc" "lb-tf-default-vpc" {
+  default = false
+  tags = {
+    Name = "lb-tf-main" # Replace this with the actual tag value that uniquely identifies your VPC
+  }
+}
 
-# resource "aws_security_group" "Project-sg" {
-#   name        = "sever1-sg"
-#   description = "My  security group"
-#   vpc_id      = data.aws_vpc.default-vpc.id  # Using the VPC ID from the data source
+resource "aws_security_group" "Project-sg" {
+  name        = "server#01-sg"
+  description = "ID#01 Security Group"
+  vpc_id      = data.aws_vpc.lb-tf-default-vpc.id # Using the VPC ID from the data source
 
   # Add rules and other security group configurations
-
-
- resource "aws_vpc" "main" {
+}
+resource "aws_vpc" "lb-tf-default-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "main"
+    Name = "lb-tf-main"
   }
 }
 
 resource "aws_subnet" "custom_subnet" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.lb-tf-default-vpc.id
   cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"  # Update with the desired availability zone
+  availability_zone       = "us-east-1a" # Update with the desired availability zone
   map_public_ip_on_launch = true
 }
 
 resource "aws_instance" "demo_instance" {
-  ami           = "ami-0a3c3a20c09d6f377"  # Update with a valid AMI ID
+  ami           = "ami-0a3c3a20c09d6f377" # Update with a valid AMI ID
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.custom_subnet.id
+  subnet_id     = aws_subnet.custom_subnet.id
+
 
   tags = {
-    Name = "example-instance"
+    Name = "tf-cert-example-instance"
   }
 }
-
-
-
-
-
-
-
-
-
